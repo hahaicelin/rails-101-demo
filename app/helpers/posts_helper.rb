f52194c1082c8cbe271066cee7ff85dcd2a69045
alice < Email>
2016-04-30 21:25:34 +0800

    for deploy heroku

---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,6 @@
+module PostsHelper
+end
+
+  def render_post_content(post)
+    simple_format(truncate(post.content, lenth: 100))
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+
+ namespace :account do
+   resources :groups
+   resources :posts
+ end
+
+  devise_for :users
+   resources :groups do
+    member do
+     post :join
+   post :quit
+   end
+
+   resources :posts
+ end
+  root 'groups#index'
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +71,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
